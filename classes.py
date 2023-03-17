2fba313870b8f98194a9835f16af1a53639ee0b5
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-03-17 19:31:30 +0530

    flight problem

---
+++ classes.py
@@ -0,0 +1,25 @@
+class Flight():
+	def __init__(self, capacity):
+		self.capacity = capacity
+		self.passengers = []
+
+	def add_passengers(self, name):
+		if not self.open_seats():
+			return False
+		self.passengers.append(name)
+		return True
+
+	def open_seats(self):
+		return self.capacity - len(self.passengers)
+
+
+
+flight = Flight(1)
+
+people = ["harry", "ben", "ron"]
+for person in people:
+	
+	if flight.add_passengers(person):
+		print(f"Added Mr.{person} to flight successfully")
+	else:
+		print("no available seats for Mr{person}")
