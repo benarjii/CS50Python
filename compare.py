6c345fcc67d6e0b26b854136a09ef568218bb6b1
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-03-20 17:16:56 +0530

    added compare.py file that compares two ints using ifs

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = int(input("enter 'x': "))
+y = int(input("enter 'y': "))
+
+if x < y:
+	print("x is less than y")
+if x > y:
+	print("x is greater than y")
+if x == y:
+	print("x is equal to y")
