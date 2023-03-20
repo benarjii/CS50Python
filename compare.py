d818d7042fadf8681b25a3ff279ca34553998971
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-03-20 17:44:23 +0530

    replaced if and elif with if and or

--- compare.py
+++ compare.py
@@ -1,9 +1,7 @@
 x = int(input("enter 'x': "))
 y = int(input("enter 'y': "))
 
-if x < y:
-	print("x is less than y")
-elif x > y:
-	print("x is greater than y")
+if x < y or x > y:
+	print("x is not equal to y")
 else:
 	print("x is equal to y")
