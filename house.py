3e7643c7cc34cc4d1c9dfe8d562b4490599ac72f
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-03-20 18:41:53 +0530

    created a conditional code using ifs, elifs anf else

---
+++ house.py
@@ -0,0 +1,10 @@
+name = input("what's your name? ")
+
+if name == "harry":
+	print("Gryffindor")
+elif name == "hermonie":
+	print("Gryffindor")
+elif name == "draco":
+	print("slytherin")
+else:
+	print("who?")
