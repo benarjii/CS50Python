885b4865a4cf91aeb05b0de1f1aeb816faef56eb
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-03-23 18:24:00 +0530

    for i in range(len(students)):

--- cat.py
+++ cat.py
@@ -1,12 +1,5 @@
 def main():
-	number = get_number()
-	meow(number)
-
-def get_number():
-	while True:
-		n = int(input("n = "))
-		if n > 0:
-			return n
+	meow(3)
 
 def meow(n):
 	for _ in range(n):
--- hogwarts.py
+++ hogwarts.py
@@ -1,6 +1,4 @@
 students = ["hogwarts", "harry", "Ron"]
 
-for student in students:
-	#print(student)
-	print(1)
-print(student)
+for i in range(len(students)):
+	print(i+1, students[i])
