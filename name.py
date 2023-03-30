2d1974967ba93d185f38d2beff49a2379fc04784
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-03-30 17:26:05 +0530

    used for arg in sys.arg: to print every arg

--- name.py
+++ name.py
@@ -2,7 +2,5 @@ import sys
 
 if len(sys.argv) < 2:
 	print("too few arguments")
-elif len(sys.argv) > 2:
-	print("too many arguments")
-else:
-	print("hello, my name is", sys.argv[1])
+for arg in sys.argv:
+	print(arg)
