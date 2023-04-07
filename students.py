348ed8cdd1da883ff71a108581f45e9bf08bc88e
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-04-07 19:20:41 +0530

    used lambda student: student['name'] by ommiting key = get_name

--- students.py
+++ students.py
@@ -7,11 +7,5 @@ with open("students.csv") as file:
 		students.append(student)
 
 
-def get_name(student):
-	return student["name"]
-
-def get_house(student):
-	return student["house"]
-
-for student in sorted(students, key = get_name, reverse = True):
+for student in sorted(students, key = lambda student : student["name"]):
 	print(f"{student['name']} is in {student['house']}")
