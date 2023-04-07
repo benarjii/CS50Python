19ce31f492d623375ff1dd8fc473e7b6e3df93fb
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-04-07 19:35:21 +0530

    instead of using split(','), csv.reader(file) is used which is present in csv library

--- students.py
+++ students.py
@@ -1,10 +1,11 @@
+import csv
+
 students = []
 
 with open("students.csv") as file:
-	for line in file:
-		name, house = line.rstrip().split(",")
-		student = {"name": name, "house": house}
-		students.append(student)
+	reader = csv.reader(file)
+	for name, house in reader:
+		students.append({"name": name, "house": house})
 
 
 for student in sorted(students, key = lambda student : student["name"]):
