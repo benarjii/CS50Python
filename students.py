1a6d0676056abc8bef7c76b8da7876cf790ef3b9
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-04-07 19:11:05 +0530

    sorted the dictionaries in the list by using the sorted(students, key = get_name)

--- students.py
+++ students.py
@@ -3,8 +3,15 @@ students = []
 with open("students.csv") as file:
 	for line in file:
 		name, house = line.rstrip().split(",")
-		students.append(f"{name} is in {house}")
+		student = {"name": name, "house": house}
+		students.append(student)
 
 
-for student in sorted(students):
-	print(student)
+def get_name(student):
+	return student["name"]
+
+def get_house(student):
+	return student["house"]
+
+for student in sorted(students, key = get_name, reverse = True):
+	print(f"{student['name']} is in {student['house']}")
