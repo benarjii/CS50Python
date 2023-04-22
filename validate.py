7d9d8dd0ae92192f5c1e63b9ce46ba0c85dc2257
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-04-22 16:49:43 +0530

    used a flag called re.IGNORECASE

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("enter email: ").strip()
 
-if re.search(r"^\w+@\w+\.(com|gov|edu|org)$", email):
+if re.search(r"^\w+@\w+\.(com|gov|edu|org)$", email, re.IGNORECASE):
 	print("valid")
 else:
 	print("Invalid")
