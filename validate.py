f1dcb31224c7c4bf76e897b197578cbb3e6651ca
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-04-22 16:39:39 +0530

    used r'^[a-zA-z0-9_]+@[a-zA-Z0-9_]+\.edu'

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("enter email: ").strip()
 
-if re.search(r"^.+@.+\.com$", email):
+if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.com$", email):
 	print("valid")
 else:
 	print("Invalid")
