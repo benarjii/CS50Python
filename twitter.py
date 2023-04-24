2fe5db63ab62ff166b61b7fc00207420b09266d6
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-04-24 16:35:29 +0530

    used re.sub(r'.*^(https?://)?(www\.)?twitter\.com/', '',url

--- twitter.py
+++ twitter.py
@@ -1,4 +1,6 @@
+import re
+
 url = input("url: ").strip()
 
-username = url.replace("https://twitter.com/", "")
+username = re.sub(r".*^(https?://)?(www\.)?twitter\.com/", "",url)
 print(f"username: {username}")
