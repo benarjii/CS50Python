d945f2358044283759ce9469a8aaee4ddaac316d
benarji mulakala <93770708+benarjii@users.noreply.github.com>
2023-04-24 16:39:46 +0530

    used re.search(r'.*^(https?://)?(www\.)?twitter\.com/(.+)$', url, re.IGNORECASE

--- twitter.py
+++ twitter.py
@@ -2,5 +2,7 @@ import re
 
 url = input("url: ").strip()
 
-username = re.sub(r".*^(https?://)?(www\.)?twitter\.com/", "",url)
-print(f"username: {username}")
+matches = re.search(r'.*^(https?://)?(www\.)?twitter\.com/(.+)$', url, re.IGNORECASE)
+
+if matches:
+	print(f"username: {matches.group(3)}")
